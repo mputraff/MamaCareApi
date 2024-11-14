10154c75e90ff0658b5137b5c2c67c78c97d07ad
putrafauzan32@gmail.com <email@example.com>
2024-11-14 19:51:05 +0700

    update api

--- middleware/authenticateToken.js
+++ middleware/authenticateToken.js
@@ -4,6 +4,12 @@ const authenticateToken = (req, res, next) => {
   const token = req.header("Authorization")?.split(" ")[1];
   if (!token) return res.status(401).json({ error: "Access denied" });
 
+  // Cek nilai dari JWT_SECRET
+  if (!process.env.JWT_SECRET) {
+    console.error("JWT_SECRET is not defined");
+    return res.status(500).json({ error: "Server configuration error" });
+  }
+
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) return res.status(403).json({ error: "Invalid token" });
     req.user = user; // Menyimpan user di `req.user`
